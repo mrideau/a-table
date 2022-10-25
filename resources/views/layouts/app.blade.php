4087f620cf71028a4b8de3d980631647328fefae
mrideau <matis.rideau@hotmail.com>
2022-10-25 18:18:29 +0200

    Suppression désactivation du zoom

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <meta name="description" соntеnt="A Table! vous proposer des recettes simples mais qui régaleront toute votre famille et vos amis !">
+    <meta name="description" content="A Table! vous proposer des recettes simples mais qui régaleront toute votre famille et vos amis !">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
