6766931257bac3bcb8de3281ec77231a41bca559
mrideau <matis.rideau@hotmail.com>
2022-10-25 10:23:52 +0200

    Fix liens mobile

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -32,9 +32,11 @@
     </nav>
 
     <ul class="sidenav" id="mobile-demo">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">Javascript</a></li>
-        <li><a href="mobile.html">Mobile</a></li>
+        <li><a href="{{ route('recipes.index') }}">{{ __('recipe.recipes') }}</a></li>
+        <li><a href="{{ route('categories.index') }}">{{ __('category.categories') }}</a></li>
+        <li><a href="{{ route('contact') }}">Contact</a></li>
+        @auth()
+            <li><a class="btn" href="{{ route('auth.logout') }}">{{ __('auth.logout') }}</a></li>
+        @endif
     </ul>
 </header>
