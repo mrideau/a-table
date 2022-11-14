5a231be47a0d21cab3beb9191da49fb7e4817804
mrideau <matis.rideau@hotmail.com>
2022-11-14 10:20:49 +0100

    Semantique liens utiles footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,7 +6,7 @@
                 <p class="grey-text text-lighten-4">Nous n'utilisons aucuns cookies ormis les cookies fonctionnels.</p>
             </div>
             <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Liens utiles</h5>
+                <p class="white-text">Liens utiles</p>
                 <ul>
                     <li><a class="grey-text text-lighten-3" href="{{ route('terms') }}">Conditions d'utilisations</a></li>
                     <li><a class="grey-text text-lighten-3" href="{{ route('legals') }}">Mentions Légales</a></li>
