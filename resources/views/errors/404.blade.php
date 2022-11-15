70a04db0fcd61e419d54f0dbb12deeace79704c9
mrideau <matis.rideau@hotmail.com>
2022-11-15 11:00:07 +0100

    Correction espace

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,8 +2,10 @@
 
 @section('content')
     <div class="container center">
-        <h1>Page Introuvable...</h1>
-        <p>Il semblerait que vous vous soyez perdu...</p>
-        <a href="/">Retourner à l'accueil</a>
+        <div class="row">
+            <h1>Page Introuvable...</h1>
+            <p>Il semblerait que vous vous soyez perdu...</p>
+            <a href="/">Retourner à l'accueil</a>
+        </div>
     </div>
 @endsection
